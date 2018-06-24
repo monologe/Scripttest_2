cc7bf487534ed41ff3b8d066a87f562e35f1f727
Sumire Kikuta <paddyfield815@gmail.com>
2018-06-24 19:20:29 +0900

    lesson5まで

---
+++ Assets/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        int a = 3;
+        if (a == 3)
+        {
+            int b = 5;
+        Debug.Log( b );}
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
